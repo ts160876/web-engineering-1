eca68d76bebc492c89470dd50a1f586c211b7950
Thorsten Schneider <mr.thorsten.schneider@outlook.com>
2025-11-22 12:35:44 +0100

    Add additional examples to lesson 7

--- demos/07/strings/index.js
+++ demos/07/strings/index.js
@@ -8,6 +8,7 @@ let age1 = 16;
 let person2 = "Struppi";
 let age2 = 7;
 
+//String concatenation
 console.log(
   person1 +
     " und " +
@@ -17,6 +18,17 @@ console.log(
     " Jahre alt."
 );
 
+//Template strings
 console.log(
   `${person1} und ${person2} sind zusammen ${age1 + age2} Jahre alt.`
 );
+
+//Repeat a string (obvious way)
+let repeated = "";
+for (let i = 1; i <= 3; i++) {
+  repeated += person1;
+}
+console.log(repeated);
+
+//Repeat a string (better way)
+console.log(person1.repeat(3));
